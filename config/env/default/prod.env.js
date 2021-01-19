2aed9293135fd589256e5d91d177e4914dae2802
Cherry <936430210@qq.com>
2021-01-19 13:49:59 +0800

    fix: 修改打包接口地址

--- config/env/default/prod.env.js
+++ config/env/default/prod.env.js
@@ -2,5 +2,5 @@ const variables = require('./variables');
 
 module.exports = Object.assign(variables, {
   NODE_ENV: 'production',
-  API_ENDPOINT: 'http://10.193.4.53:8283/api/v1/',
+  API_ENDPOINT: 'http://192.168.101.119:10024/',
 });
